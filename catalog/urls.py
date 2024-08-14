f16c63fc491c66ef562f9b6c7478081b7e3dc0b7
Dmitriy <smank96@gmail.com>
2024-08-14 12:01:43 +0700

    Добавлены импорты settings, static. Добавлена настройка с указанием директории хранения медиа файлов.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from catalog.apps import MainappConfig
 from catalog.views import home, contacts
@@ -7,4 +9,4 @@ app_name = MainappConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
