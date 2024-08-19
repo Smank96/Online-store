3a46b87417a1fdeac0fdfe6bb28367f1c06eba52
Dmitriy <smank96@gmail.com>
2024-08-19 22:00:49 +0700

    Выделил базовый шаблон и подшаблоны с главным меню, футером, пагинатором. Вывел список товаров в цикле на странице products. Создал новый контроллер и шаблон, которые отвечают за отображение отдельной страницы с товаром. Реализовал шаблонный фильтр, который преобразует переданный путь в полный путь для доступа к медиафайлу. Реализовал постраничный вывод списка продуктов. Добавил функционал создания продукта через отдельную страницу.

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from catalog.apps import MainappConfig
-from catalog.views import home, contacts
+from catalog.views import products, product_details, contacts, new_product
 
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('', products, name='home'),
+    path('products/<int:pk>/', product_details, name='product_details'),
+    path('page/<int:page_number>/', products, name='paginator'),
+    path('contacts/', contacts, name='contacts'),
+    path('new_product/', new_product, name='new_product')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from catalog.models import Contacts
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Contacts, Product
+from django.core.paginator import Paginator
 
 
-def home(request):
-    return render(request, 'home.html')
+def products(request, page_number=1):
+    products_set = Product.objects.all()
+
+    per_page = 6
+    paginator = Paginator(products_set, per_page)
+    products_paginator = paginator.page(page_number)
+
+    context = {"products": products_paginator}
+    return render(request, 'products.html', context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_details.html', context)
 
 
 def contacts(request):
@@ -16,3 +30,18 @@ def contacts(request):
         Contacts.objects.create(name=name, phone_number=phone_number, message=message)
     return render(request, 'contacts.html')
 
+
+def new_product(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product_name')
+        product_description = request.POST.get('product_description')
+        product_picture = request.POST.get('product_picture')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+
+        Product.objects.create(product_name=product_name,
+                               product_description=product_description,
+                               product_picture=product_picture,
+                               category=category,
+                               price=price)
+    return render(request, 'new_product.html')
